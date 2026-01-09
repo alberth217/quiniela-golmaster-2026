d69762bcc0c6d0dd831f07802b3cc45fa0cb77ef
amedina <medinaalberth3@gmail.com>
2026-01-09 11:11:35 -0400

    URL original a al backend de predigol

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-const config = {
-    API_URL: import.meta.env.VITE_API_URL || 'https://alberth217-quiniela-2026-m5gd.vercel.app'
-};
+const API_URL = import.meta.env.VITE_API_URL || 'https://alberth217-quiniela-2026-m5gd.vercel.app';
 
-export default config;
+export default API_URL;
